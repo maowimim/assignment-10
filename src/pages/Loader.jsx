262649a51ed1e420083583984d790107222f7b59
maowimim <maowa087@gmail.com>
2025-12-07 23:45:45 +0600

    add

--- src/pages/Loader.jsx
+++ src/pages/Loader.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Loader = () => {
     return (
-      
+        <div className='min-h-screen flex justify-center items-center'>
+            <span className="loading loading-spinner loading-xl"></span>
+            
+        </div> 
     );
 };
 
