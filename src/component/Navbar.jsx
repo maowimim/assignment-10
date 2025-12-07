afcd0b464726a7059df99553dff2bdf0ae127be0
maowimim <maowa087@gmail.com>
2025-12-07 21:40:00 +0600

    add plants

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,26 +1,25 @@
-import React, { useContext } from 'react';
+import React, { use } from 'react'
 import { Link } from 'react-router';
 import { AuthContext } from '../Provider/AuthProvider';
 
 const Navbar = () => {
-  const { user, userlogout } = useContext(AuthContext);
+  const { user, userlogout } = use(AuthContext)
 
-  const handleSignOut = () => {
-    userlogout()
-      .then(() => { })
-      .catch(error => {
-        console.log(error.message);
-      });
-  };
+  const handleLogout = () => {
+     userlogout()
+      .then(()=>{
 
+      })
+      .catch(error=>{
+        console.log(error.message)
+      })
+  }
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
           </div>
           <ul
             tabIndex="-1"
@@ -28,12 +27,10 @@ const Navbar = () => {
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/plantdata'>Plants</Link></li>
             <li><Link to="/myprofile">My Profile</Link></li>
-            <li><Link to="/add-plants">Add Plants</Link></li>
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">Flower SHOP</a>
       </div>
-
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li><Link to='/'>Home</Link></li>
@@ -42,28 +39,37 @@ const Navbar = () => {
           <li><Link to="/add-plants">Add Plants</Link></li>
         </ul>
       </div>
-
       <div className="navbar-end">
-        {user ? (
-          <button
-            onClick={handleSignOut}
-            className="px-6 py-2 font-semibold text-white rounded-lg bg-gradient-to-r from-blue-600 via-indigo-500 to-purple-600 
-            hover:from-purple-600 hover:via-indigo-500 hover:to-blue-600 transition-all duration-300 shadow-lg hover:shadow-xl"
-          >
-            Logout
-          </button>
-        ) : (
-          <Link
-            to={"/Login"}
-            className="px-6 py-2 font-semibold text-white rounded-lg bg-gradient-to-r from-blue-600 via-indigo-500 to-purple-600 
-            hover:from-purple-600 hover:via-indigo-500 hover:to-blue-600 transition-all duration-300 shadow-lg hover:shadow-xl"
-          >
-            Login
-          </Link>
+        {
+          user && (
+          <div>
+            <button
+              onClick={handleLogout}
+              className="px-6 py-2 font-semibold text-white rounded-lg bg-gradient-to-r from-blue-600 via-indigo-500 to-purple-600 hover:from-purple-600 hover:via-indigo-500 hover:to-blue-600 transition-all duration-300 shadow-lg hover:shadow-xl"
+            >
+              Logout
+            </button>
+            </div>
+            ) 
+        }
+
+       {!user && (
+          <div >
+            <Link
+              to={"/Login"}
+              className="px-6 py-2 font-semibold text-white rounded-lg bg-gradient-to-r from-blue-600 via-indigo-500 to-purple-600 hover:from-purple-600 hover:via-indigo-500 hover:to-blue-600 transition-all duration-300 shadow-lg hover:shadow-xl"
+            >
+              Login
+            </Link>
+          </div>
         )}
+
+
+
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
+
